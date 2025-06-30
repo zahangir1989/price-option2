c2c9041c3fde34f2f5e5974d01832b1da96b5746
zahangir <alamzahangir218@gmail.com>
2025-06-30 07:59:21 +0600

    this is first commit of price option

--- src/component/Link/Link.jsx
+++ src/component/Link/Link.jsx
@@ -3,7 +3,7 @@
 const Link = ({route}) => {
     return (
        
-            <li className="mr-10"> <a href={route.path}>{route.name}</a></li>
+            <li className="mr-10"> <a href={route.path}>{route.name}</a></li>git remote add origin https://github.com/zahangir1989/price-option2.git
             
        
     );
