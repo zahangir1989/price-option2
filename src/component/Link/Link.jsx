2dfc867504d26914801af2bc93206c831c2d204f
zahangir <alamzahangir218@gmail.com>
2025-06-30 07:54:20 +0600

    first commit is price option

---
+++ src/component/Link/Link.jsx
@@ -0,0 +1,12 @@
+
+
+const Link = ({route}) => {
+    return (
+       
+            <li className="mr-10"> <a href={route.path}>{route.name}</a></li>
+            
+       
+    );
+};
+
+export default Link;
